dbb3845d5f785dc08db669822fee85b928bde037
Pundhi Yuliawati <36281730+pyuliaw23@users.noreply.github.com>
2018-02-16 05:45:07 +0700

    Add files via upload

---
+++ Nilai.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Nilai : MonoBehaviour
+{
+    public GameObject enemy;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "tongSampah":
+                Destroy(this.gameObject);
+                Debug.Log("Masuk");
+                break;
+            case "Plan":
+                Destroy(this.gameObject);
+                Debug.Log("Tidak Masuk");
+                break;
+        }
+
+    }
+
+
+}
---
+++ TongSampahController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TongSampahController : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            GoLeft();
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            GoRight();
+        }
+    }
+    
+    private void GoLeft(){
+        transform.Translate (Vector3.left * speed * Time.deltaTime);
+    }
+    private void GoRight(){
+        transform.Translate (Vector3.right * speed * Time.deltaTime);
+    }
+
+   
+}
